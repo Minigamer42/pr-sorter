956ce3c515114a5348223156238c51c37938219e
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2026-05-18 19:42:17 +0200

    wuwa

--- customize/config.ts
+++ customize/config.ts
@@ -1,14 +1,16 @@
 import type { AppConfig } from '../src/app/types';
 
 export const config = {
-    localStoragePrefix: 'test',
-    title: 'test! Sorter',
-    description: 'Party rank sorter for your custom list of songs.',
-    deadline: new Date('2026-06-30T23:59:00+02:00'),
+    localStoragePrefix: 'wuwa-ost',
+    title: 'WuWa OST',
+    description: 'Party rank sorter for WuWa OST',
+    fallbackAnimeName: 'WuWa',
+    deadline: new Date('2026-05-31T12:00:00+02:00'),
+    tags: ['Franchise'],
     googleSheets: {
         clientId: '575550662002-hivobiln683gua375ss3b7k58afnn36t.apps.googleusercontent.com',
         appId: '575550662002',
         rankColumnHeader: 'Rank',
-        scoreColumnHeader: 'Score (optional)'
+        scoreColumnHeader: 'Score'
     }
 } satisfies AppConfig;
