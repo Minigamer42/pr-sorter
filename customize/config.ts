95ad6501ff985d7a6d47f8a037aceaf7c07357da
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2026-05-25 00:19:06 +0200

    mili

--- customize/config.ts
+++ customize/config.ts
@@ -1,11 +1,12 @@
 import type { AppConfig } from '../src/app/types';
 
 export const config = {
-    localStoragePrefix: 'test',
-    title: 'test! Sorter',
-    description: 'Party rank sorter for your custom list of songs.',
-    tags: ['test'],
-    deadline: new Date('2026-06-30T23:59:00+02:00'),
+    localStoragePrefix: 'mili',
+    title: 'Mili Sorter',
+    description: 'Party rank sorter for Mili',
+    fallbackAnimeName: 'Mili',
+    tags: ['Artist'],
+    hide: true,
     googleSheets: {
         clientId: '575550662002-hivobiln683gua375ss3b7k58afnn36t.apps.googleusercontent.com',
         appId: '575550662002',
