b591ec6f0e2b1f66c270b87533a9081952cf5633
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2026-05-30 16:25:51 +0200

    Cö shu Nie

--- customize/config.ts
+++ customize/config.ts
@@ -1,14 +1,15 @@
 import type { AppConfig } from '../src/app/types';
 
 export const config = {
-    localStoragePrefix: 'test',
-    title: 'test! Sorter',
-    description: 'Party rank sorter for your custom list of songs.',
-    deadline: new Date('2026-06-30T23:59:00+02:00'),
+    localStoragePrefix: 'co-shu-nie',
+    title: 'Cö shu Nie Sorter',
+    description: 'Party rank sorter for Cö shu Nie',
+    deadline: new Date('2026-06-05T23:59:00+02:00'),
+    tags: ['Artist'],
     googleSheets: {
         clientId: '575550662002-hivobiln683gua375ss3b7k58afnn36t.apps.googleusercontent.com',
         appId: '575550662002',
         rankColumnHeader: 'Rank',
-        scoreColumnHeader: 'Score (optional)'
+        scoreColumnHeader: 'Score'
     }
 } satisfies AppConfig;
