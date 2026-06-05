7b6dc3f56588a9e1c19b04983e9c8f1984e82556
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2026-06-05 02:41:12 +0200

    bring back import route in dev mode

--- src/routes/PagesPreviewRoute.tsx
+++ src/routes/PagesPreviewRoute.tsx
@@ -1,8 +1,17 @@
 import { SorterIndex } from "../sorterIndex/SorterIndex";
+import { CustomizeImportRoute } from "./CustomizeImportRoute";
 import { ActiveRoute as SorterAppRoute } from "./SorterAppRoute";
 
 export function ActiveRoute() {
+  if (isCustomizeImportRoute()) {
+    return <CustomizeImportRoute />;
+  }
+
   return isSorterRoute() ? <SorterAppRoute /> : <SorterIndex />;
+}
+
+function isCustomizeImportRoute(): boolean {
+  return window.location.pathname.replace(/\/+$/, "") === "/import";
 }
 
 function isSorterRoute(): boolean {
