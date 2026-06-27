a82ceca0009ae15037fde7c9b74a59d3dd202b0f
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2026-06-27 11:44:33 +0200

    suara

--- customize/config.ts
+++ customize/config.ts
@@ -1,15 +1,15 @@
 import type { AppConfig } from '../src/app/types';
 
 export const config = {
-    localStoragePrefix: 'test',
-    title: 'test! Sorter',
-    description: 'Party rank sorter for your custom list of songs.',
-    tags: ['test'],
-    deadline: new Date('2026-06-30T23:59:00+02:00'),
+    localStoragePrefix: 'suara',
+    title: 'Suara Sorter',
+    description: 'Party rank sorter for Suara',
+    tags: ['Artist'],
+    deadline: new Date('2026-07-08T06:00:00+02:00'),
     googleSheets: {
         clientId: '575550662002-hivobiln683gua375ss3b7k58afnn36t.apps.googleusercontent.com',
         appId: '575550662002',
         rankColumnHeader: 'Rank',
-        scoreColumnHeader: 'Score (optional)'
+        scoreColumnHeader: 'Score'
     }
 } satisfies AppConfig;
--- customize/songList.ts
+++ customize/songList.ts
@@ -3,34 +3,260 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {'id': 1, 'anime': 'Bang Dream', 'name': 'Asu no Yozora Shoukaihan by Afterglow', 'video': 'https://www.youtube.com/watch?v=1JgfLP2QtZw', 'mp3': null},
-    {'id': 2, 'anime': 'Bang Dream', 'name': 'Ave Mujica by Ave Mujica', 'video': 'https://youtu.be/QDsd0nyzwz0', 'mp3': null},
-    {'id': 3, 'anime': 'Bang Dream', 'name': 'Bad Kids All Bet by RAISE A SUILEN', 'video': 'https://www.youtube.com/watch?v=mKt2u5a3-H8', 'mp3': null},
-    {'id': 4, 'anime': 'Bang Dream', 'name': 'Beautiful Birthday by RAISE A SUILEN', 'video': 'https://www.youtube.com/watch?v=AxEUw6LdHR4', 'mp3': null},
-    {'id': 5, 'anime': 'Bang Dream', 'name': 'Choir \'s\' Choir by Ave Mujica', 'video': 'https://youtu.be/7llj4mh4L-8', 'mp3': null},
-    {'id': 6, 'anime': 'Bang Dream', 'name': 'Double Rainbow by Poppin\'Party', 'video': 'https://www.youtube.com/watch?v=sNaJuqFB-_g', 'mp3': null},
-    {'id': 7, 'anime': 'Bang Dream', 'name': 'Ether by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=z6k7YIIZ6Hk', 'mp3': null},
-    {'id': 8, 'anime': 'Bang Dream', 'name': 'Freedom by Poppin\'Party x Misato Fukuen', 'video': 'https://www.youtube.com/watch?v=oy6OCnr9eAg', 'mp3': null},
-    {'id': 9, 'anime': 'Bang Dream', 'name': 'Hey-day Capriccio by Afterglow', 'video': 'https://www.youtube.com/watch?v=czHlLBls4oo', 'mp3': null},
-    {'id': 10, 'anime': 'Bang Dream', 'name': 'Kamisama, Baka by Ave Mujica', 'video': 'https://youtu.be/vmheOfqsH_o', 'mp3': null},
-    {'id': 11, 'anime': 'Bang Dream', 'name': 'KiLLKiSS by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=RexyoaXaQ1o', 'mp3': null},
-    {'id': 12, 'anime': 'Bang Dream', 'name': 'Kimi ga Inakucha, Hello Happy World', 'video': 'https://naedist.animemusicquiz.com/8poyjw.webm', 'mp3': null},
-    {'id': 13, 'anime': 'Bang Dream', 'name': 'Mugen My World by Mugendai Mewtype', 'video': 'https://youtube.com/watch?v=GB2MEvY2sQk', 'mp3': null},
-    {'id': 14, 'anime': 'Bang Dream', 'name': 'Radiate Warriors by Pastel＊Palettes', 'video': 'https://youtu.be/dS-SUwftzmM', 'mp3': null},
-    {'id': 15, 'anime': 'Bang Dream', 'name': 'Returns by Poppin\'Party', 'video': 'https://www.youtube.com/watch?v=zWKV5yudE18', 'mp3': null},
-    {'id': 16, 'anime': 'Bang Dream', 'name': 'Roku-chou Nen to Ichiya Monogatari by Roselia', 'video': 'https://www.youtube.com/watch?v=xnlJkmRhi3Q', 'mp3': null},
-    {'id': 17, 'anime': 'Bang Dream', 'name': 'Sing Alive by Roselia', 'video': 'https://naedist.animemusicquiz.com/2h5qt3.webm', 'mp3': null},
-    {'id': 18, 'anime': 'Bang Dream', 'name': 'Mayoi Uta by MyGO!!!!!', 'video': 'https://naedist.animemusicquiz.com/vppf6y.webm', 'mp3': null},
-    {'id': 19, 'anime': 'Bang Dream', 'name': 'Smiling & Singing A Song by Hello, Happy World!', 'video': 'https://youtu.be/XFv3P-fKhWw', 'mp3': null},
-    {'id': 20, 'anime': 'Bang Dream', 'name': 'Song I am. by Roselia', 'video': 'https://naedist.animemusicquiz.com/njc8p5.webm', 'mp3': null},
-    {'id': 21, 'anime': 'Bang Dream', 'name': 'Sora no Música by Ave Mujica', 'video': 'https://youtu.be/2Bo-ULStqLo', 'mp3': null},
-    {'id': 22, 'anime': 'Bang Dream', 'name': 'Sprechchor by Roselia', 'video': 'https://www.youtube.com/watch?v=sdRwP4OjVn4', 'mp3': null},
-    {'id': 23, 'anime': 'Bang Dream', 'name': 'Sunlit Musical - Roselia', 'video': 'https://youtu.be/BtG1mA_1e0s', 'mp3': null},
-    {'id': 24, 'anime': 'Bang Dream', 'name': 'swim by MyGO!!!!!', 'video': 'https://www.youtube.com/watch?v=AEZ7suhPML0', 'mp3': null},
-    {'id': 25, 'anime': 'Bang Dream', 'name': 'Symbol III: ▽ by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=NB3PxWLn9v4', 'mp3': null},
-    {'id': 26, 'anime': 'Bang Dream', 'name': 'Symbol IV :Earth by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=WqQPM0dZpXc', 'mp3': null},
-    {'id': 27, 'anime': 'Bang Dream', 'name': 'Telepathy by Mugendai Mewtype', 'video': 'https://www.youtube.com/watch?v=GN_0T7cClGM', 'mp3': null},
-    {'id': 28, 'anime': 'Bang Dream', 'name': 'The Circle of Butterflies by Morfonica x fhána', 'video': 'https://www.youtube.com/watch?v=JfhgKn1hnnk', 'mp3': null},
-    {'id': 29, 'anime': 'Bang Dream', 'name': 'Utakotoba by MyGO!!!!!', 'video': 'https://youtu.be/wJ-OebTVyvk', 'mp3': null},
-    {'id': 30, 'anime': 'Bang Dream', 'name': 'Yakusoku by Roselia', 'video': 'https://naedist.animemusicquiz.com/h0rza6.webm', 'mp3': null},
+    {
+        'id': 1,
+        'anime': 'Utawarerumono',
+        'name': 'Musouka by Suara',
+        'video': 'https://naedist.animemusicquiz.com/t5bzh7.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/kksj9x.mp3',
+        'full': null
+    },
+    {
+        'id': 2,
+        'anime': 'Utawarerumono',
+        'name': 'Kimi ga Tame by Suara',
+        'video': 'https://naedist.animemusicquiz.com/34njnq.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/au342f.mp3',
+        'full': null
+    },
+    {
+        'id': 3,
+        'anime': 'To Heart 2',
+        'name': 'Tomoshibi by Suara',
+        'video': 'https://naedist.animemusicquiz.com/op30qu.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/c0j3hq.mp3',
+        'full': null
+    },
+    {
+        'id': 4,
+        'anime': 'Asatte no Houkou.',
+        'name': 'Hikari no Kisetsu by Suara',
+        'video': 'https://naedist.animemusicquiz.com/anti78.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/iwa50r.mp3',
+        'full': null
+    },
+    {
+        'id': 5,
+        'anime': 'Asatte no Houkou.',
+        'name': 'Kasa by Suara',
+        'video': 'https://naedist.animemusicquiz.com/om88x1.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/gr7m3l.mp3',
+        'full': null
+    },
+    {
+        'id': 6,
+        'anime': 'To Heart 2 OVA',
+        'name': 'Ichibanboshi by Suara',
+        'video': 'https://naedist.animemusicquiz.com/un9669.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/4xi5v2.mp3',
+        'full': null
+    },
+    {
+        'id': 7,
+        'anime': 'To Heart 2 OVA',
+        'name': 'I am by Suara',
+        'video': 'https://naedist.animemusicquiz.com/cl9vwn.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/z796bh.mp3',
+        'full': null
+    },
+    {
+        'id': 8,
+        'anime': 'KimiKiss Pure Rouge',
+        'name': 'Wasurenaide by Suara',
+        'video': 'https://naedist.animemusicquiz.com/5y6j57.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/vhly7y.mp3',
+        'full': null
+    },
+    {
+        'id': 9,
+        'anime': 'Blue Drop: Tenshi-tachi no Gikyoku',
+        'name': 'BLUE by Suara',
+        'video': 'https://naedist.animemusicquiz.com/y5qgj6.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/px0ibz.mp3',
+        'full': null
+    },
+    {
+        'id': 10,
+        'anime': 'Blue Drop: Tenshi-tachi no Gikyoku',
+        'name': 'Tsubomi -blue dreams- by Suara',
+        'video': 'https://naedist.animemusicquiz.com/67s9d7.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/0pceg9.mp3',
+        'full': null
+    },
+    {
+        'id': 11,
+        'anime': 'Utawarerumono OVA',
+        'name': 'adamant faith by Suara',
+        'video': 'https://naedist.animemusicquiz.com/028uaf.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/zrpwrf.mp3',
+        'full': null
+    },
+    {
+        'id': 12,
+        'anime': 'Tears to Tiara',
+        'name': 'Free and Dream by Suara',
+        'video': 'https://naedist.animemusicquiz.com/e6m1nj.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/aq2ve7.mp3',
+        'full': null
+    },
+    {
+        'id': 13,
+        'anime': 'Tears to Tiara',
+        'name': 'memory by Suara',
+        'video': 'https://naedist.animemusicquiz.com/i54k1f.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/ryir0v.mp3',
+        'full': null
+    },
+    {
+        'id': 14,
+        'anime': 'White Album',
+        'name': 'Maiochiru Yuki no You ni by Suara',
+        'video': 'https://naedist.animemusicquiz.com/l2vxaa.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/wd845j.mp3',
+        'full': null
+    },
+    {
+        'id': 15,
+        'anime': 'White Album',
+        'name': 'Akai Ito by Suara',
+        'video': 'https://naedist.animemusicquiz.com/vnkkzy.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/eb5hp3.mp3',
+        'full': null
+    },
+    {
+        'id': 16,
+        'anime': 'To Heart 2 Dungeon Travelers',
+        'name': 'Niji-iro no Mirai by Suara',
+        'video': 'https://naedist.animemusicquiz.com/ll4gsz.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/wat5gn.mp3',
+        'full': null
+    },
+    {
+        'id': 17,
+        'anime': 'Cardfight!! Vanguard: Link Joker-hen',
+        'name': 'Fly away -Oozora e- by Suara',
+        'video': 'https://naedist.animemusicquiz.com/xvk5ci.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/sv1qy1.mp3',
+        'full': null
+    },
+    {
+        'id': 18,
+        'anime': 'Future Card Buddyfight',
+        'name': 'Card of the Future by Psychic Lover×Suara',
+        'video': 'https://naedist.animemusicquiz.com/qk68o6.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/r00ur5.mp3',
+        'full': null
+    },
+    {
+        'id': 19,
+        'anime': 'Cardfight!! Vanguard: Legion Mate-hen',
+        'name': 'V-ROAD by BUSHI★7',
+        'video': 'https://naedist.animemusicquiz.com/k0wy80.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/jgszjl.mp3',
+        'full': null
+    },
+    {
+        'id': 20,
+        'anime': 'Gekijouban Cardfight!! Vanguard: Neon Messiah',
+        'name': 'V-ROAD (symphony ver.) by BUSHI★7',
+        'video': 'https://naedist.animemusicquiz.com/rwjgvl.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/tsqc2l.mp3',
+        'full': null
+    },
+    {
+        'id': 21,
+        'anime': 'Utawarerumono: Itsuwari no Kamen',
+        'name': 'Fuantei na Kamisama by Suara',
+        'video': 'https://naedist.animemusicquiz.com/pbzsm6.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/5kne9a.mp3',
+        'full': null
+    },
+    {
+        'id': 22,
+        'anime': 'Utawarerumono: Itsuwari no Kamen',
+        'name': 'Amakakeru Hoshi by Suara',
+        'video': 'https://naedist.animemusicquiz.com/yg4voo.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/347uaj.mp3',
+        'full': null
+    },
+    {
+        'id': 23,
+        'anime': 'Utawarerumono: Itsuwari no Kamen',
+        'name': 'Yume ka Utsutsu ka by Suara',
+        'video': 'https://naedist.animemusicquiz.com/46huhp.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/x1r5nb.mp3',
+        'full': null
+    },
+    {
+        'id': 24,
+        'anime': 'Utawarerumono: Itsuwari no Kamen',
+        'name': 'Hoshi Furu Sora Aogimite by Suara',
+        'video': 'https://naedist.animemusicquiz.com/o4nx06.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/34gtgq.mp3',
+        'full': null
+    },
+    {
+        'id': 25,
+        'anime': 'Utawarerumono: Itsuwari no Kamen',
+        'name': 'Nuedori by Suara',
+        'video': 'https://naedist.animemusicquiz.com/k5qy4k.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/38cen2.mp3',
+        'full': null
+    },
+    {
+        'id': 26,
+        'anime': 'Nil Admirari no Tenbin',
+        'name': 'Homura no Tori by Suara',
+        'video': 'https://naedist.animemusicquiz.com/mbg39i.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/nuco8i.mp3',
+        'full': null
+    },
+    {
+        'id': 27,
+        'anime': 'Utawarerumono: Futari no Hakuoro',
+        'name': 'Hito Nanda by Suara',
+        'video': 'https://naedist.animemusicquiz.com/13nqxw.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/zwv5k4.mp3',
+        'full': null
+    },
+    {
+        'id': 28,
+        'anime': 'Utawarerumono: Futari no Hakuoro',
+        'name': 'Hyakunichisou by Suara',
+        'video': 'https://naedist.animemusicquiz.com/1mjtmp.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/ts9mui.mp3',
+        'full': null
+    },
+    {
+        'id': 29,
+        'anime': 'Utawarerumono: Futari no Hakuoro',
+        'name': 'Musouka 2016 by Suara',
+        'video': 'https://naedist.animemusicquiz.com/b07ei8.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/yjs1u8.mp3',
+        'full': null
+    },
+    {
+        'id': 30,
+        'anime': 'Utawarerumono: Futari no Hakuoro',
+        'name': 'Koiyume by Suara',
+        'video': 'https://naedist.animemusicquiz.com/dv6vr3.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/mwnhmx.mp3',
+        'full': null
+    },
+    {
+        'id': 31,
+        'anime': 'Utawarerumono: Futari no Hakuoro',
+        'name': 'Hikari by Suara',
+        'video': 'https://naedist.animemusicquiz.com/gc29hq.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/8rh10g.mp3',
+        'full': null
+    },
+    {
+        'id': 32,
+        'anime': 'Utawarerumono: Futari no Hakuoro',
+        'name': 'Kimi ga Tame 2016 by Suara',
+        'video': 'https://naedist.animemusicquiz.com/nllynv.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/jxm0ux.mp3',
+        'full': null
+    }
 ] satisfies Song[];
