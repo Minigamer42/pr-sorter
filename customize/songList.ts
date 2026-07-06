bf1d125992df785f35cf6d5e280bd11b980c2f96
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2026-07-06 22:08:39 +0200

    vsinger originals

--- customize/songList.ts
+++ customize/songList.ts
@@ -3,34 +3,484 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {'id': 1, 'anime': 'Bang Dream', 'name': 'Asu no Yozora Shoukaihan by Afterglow', 'video': 'https://www.youtube.com/watch?v=1JgfLP2QtZw', 'mp3': null},
-    {'id': 2, 'anime': 'Bang Dream', 'name': 'Ave Mujica by Ave Mujica', 'video': 'https://youtu.be/QDsd0nyzwz0', 'mp3': null},
-    {'id': 3, 'anime': 'Bang Dream', 'name': 'Bad Kids All Bet by RAISE A SUILEN', 'video': 'https://www.youtube.com/watch?v=mKt2u5a3-H8', 'mp3': null},
-    {'id': 4, 'anime': 'Bang Dream', 'name': 'Beautiful Birthday by RAISE A SUILEN', 'video': 'https://www.youtube.com/watch?v=AxEUw6LdHR4', 'mp3': null},
-    {'id': 5, 'anime': 'Bang Dream', 'name': 'Choir \'s\' Choir by Ave Mujica', 'video': 'https://youtu.be/7llj4mh4L-8', 'mp3': null},
-    {'id': 6, 'anime': 'Bang Dream', 'name': 'Double Rainbow by Poppin\'Party', 'video': 'https://www.youtube.com/watch?v=sNaJuqFB-_g', 'mp3': null},
-    {'id': 7, 'anime': 'Bang Dream', 'name': 'Ether by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=z6k7YIIZ6Hk', 'mp3': null},
-    {'id': 8, 'anime': 'Bang Dream', 'name': 'Freedom by Poppin\'Party x Misato Fukuen', 'video': 'https://www.youtube.com/watch?v=oy6OCnr9eAg', 'mp3': null},
-    {'id': 9, 'anime': 'Bang Dream', 'name': 'Hey-day Capriccio by Afterglow', 'video': 'https://www.youtube.com/watch?v=czHlLBls4oo', 'mp3': null},
-    {'id': 10, 'anime': 'Bang Dream', 'name': 'Kamisama, Baka by Ave Mujica', 'video': 'https://youtu.be/vmheOfqsH_o', 'mp3': null},
-    {'id': 11, 'anime': 'Bang Dream', 'name': 'KiLLKiSS by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=RexyoaXaQ1o', 'mp3': null},
-    {'id': 12, 'anime': 'Bang Dream', 'name': 'Kimi ga Inakucha, Hello Happy World', 'video': 'https://naedist.animemusicquiz.com/8poyjw.webm', 'mp3': null},
-    {'id': 13, 'anime': 'Bang Dream', 'name': 'Mugen My World by Mugendai Mewtype', 'video': 'https://youtube.com/watch?v=GB2MEvY2sQk', 'mp3': null},
-    {'id': 14, 'anime': 'Bang Dream', 'name': 'Radiate Warriors by Pastel＊Palettes', 'video': 'https://youtu.be/dS-SUwftzmM', 'mp3': null},
-    {'id': 15, 'anime': 'Bang Dream', 'name': 'Returns by Poppin\'Party', 'video': 'https://www.youtube.com/watch?v=zWKV5yudE18', 'mp3': null},
-    {'id': 16, 'anime': 'Bang Dream', 'name': 'Roku-chou Nen to Ichiya Monogatari by Roselia', 'video': 'https://www.youtube.com/watch?v=xnlJkmRhi3Q', 'mp3': null},
-    {'id': 17, 'anime': 'Bang Dream', 'name': 'Sing Alive by Roselia', 'video': 'https://naedist.animemusicquiz.com/2h5qt3.webm', 'mp3': null},
-    {'id': 18, 'anime': 'Bang Dream', 'name': 'Mayoi Uta by MyGO!!!!!', 'video': 'https://naedist.animemusicquiz.com/vppf6y.webm', 'mp3': null},
-    {'id': 19, 'anime': 'Bang Dream', 'name': 'Smiling & Singing A Song by Hello, Happy World!', 'video': 'https://youtu.be/XFv3P-fKhWw', 'mp3': null},
-    {'id': 20, 'anime': 'Bang Dream', 'name': 'Song I am. by Roselia', 'video': 'https://naedist.animemusicquiz.com/njc8p5.webm', 'mp3': null},
-    {'id': 21, 'anime': 'Bang Dream', 'name': 'Sora no Música by Ave Mujica', 'video': 'https://youtu.be/2Bo-ULStqLo', 'mp3': null},
-    {'id': 22, 'anime': 'Bang Dream', 'name': 'Sprechchor by Roselia', 'video': 'https://www.youtube.com/watch?v=sdRwP4OjVn4', 'mp3': null},
-    {'id': 23, 'anime': 'Bang Dream', 'name': 'Sunlit Musical - Roselia', 'video': 'https://youtu.be/BtG1mA_1e0s', 'mp3': null},
-    {'id': 24, 'anime': 'Bang Dream', 'name': 'swim by MyGO!!!!!', 'video': 'https://www.youtube.com/watch?v=AEZ7suhPML0', 'mp3': null},
-    {'id': 25, 'anime': 'Bang Dream', 'name': 'Symbol III: ▽ by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=NB3PxWLn9v4', 'mp3': null},
-    {'id': 26, 'anime': 'Bang Dream', 'name': 'Symbol IV :Earth by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=WqQPM0dZpXc', 'mp3': null},
-    {'id': 27, 'anime': 'Bang Dream', 'name': 'Telepathy by Mugendai Mewtype', 'video': 'https://www.youtube.com/watch?v=GN_0T7cClGM', 'mp3': null},
-    {'id': 28, 'anime': 'Bang Dream', 'name': 'The Circle of Butterflies by Morfonica x fhána', 'video': 'https://www.youtube.com/watch?v=JfhgKn1hnnk', 'mp3': null},
-    {'id': 29, 'anime': 'Bang Dream', 'name': 'Utakotoba by MyGO!!!!!', 'video': 'https://youtu.be/wJ-OebTVyvk', 'mp3': null},
-    {'id': 30, 'anime': 'Bang Dream', 'name': 'Yakusoku by Roselia', 'video': 'https://naedist.animemusicquiz.com/h0rza6.webm', 'mp3': null},
+    {
+        'id': 1,
+        'anime': 'Nornis',
+        'name': 'Try add',
+        'video': 'https://youtu.be/yBfNUR30q9M?si=Sxz8oYx00JRUXw7W',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 2,
+        'anime': 'Hakui Koyori',
+        'name': 'Kanseitou Yori',
+        'video': 'https://youtu.be/-4ZiokRXZ6g?si=3qPYgy5HaELeZRwi',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 3,
+        'anime': 'Rain Drops',
+        'name': 'Amakotoba',
+        'video': 'https://youtu.be/GhlUnZdyo5Q?si=8knXgyar4sDafcch',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 4,
+        'anime': 'neko',
+        'name': 'Wonderland',
+        'video': 'https://youtu.be/-aSyaNZ2lMM',
+        'mp3': 'https://files.catbox.moe/euxrid.mp3',
+        'full': null
+    },
+    {
+        'id': 5,
+        'anime': 'Megatera Zero',
+        'name': '34yey',
+        'video': 'https://youtu.be/li1ieSSzztA',
+        'mp3': 'https://files.catbox.moe/9b3jth.mp3',
+        'full': null
+    },
+    {
+        'id': 6,
+        'anime': 'Meychan',
+        'name': 'Mononoke no Tagui',
+        'video': 'https://youtu.be/5AeYG6reVzo',
+        'mp3': 'https://files.catbox.moe/rvmsm9.mp3',
+        'full': null
+    },
+    {
+        'id': 7,
+        'anime': 'Ookami Mio',
+        'name': 'NOROSHI',
+        'video': 'https://youtu.be/WWJ90ICwC1Y?si=n6CUhQGyteXDebDt',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 8,
+        'anime': 'Ninomae Ina\'nis',
+        'name': 'TEMARI',
+        'video': 'https://youtu.be/LtTO1HMGjqA?si=4PfaA1mczDeEK0ID',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 9,
+        'anime': 'KOKO',
+        'name': 'Kirisaku',
+        'video': 'https://youtu.be/9piDtCdMtTA?si=q6Pi4UvlDeC6HMcC',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 10,
+        'anime': 'Mori Calliope',
+        'name': 'Overkill',
+        'video': 'https://www.youtube.com/watch?v=J7tp_0lFI0I',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 11,
+        'anime': 'Shiranui Flare',
+        'name': 'Daikangensai! Yume no Flare Channel!',
+        'video': 'https://www.youtube.com/watch?v=iH6_WbFsI3g',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 12,
+        'anime': 'miComet',
+        'name': 'Lollipop',
+        'video': 'https://www.youtube.com/watch?v=Jz2SeJDfml8&pp=ygUQTG9sbGlwb3AgbWljb21ldA%3D%3D',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 13,
+        'anime': 'HIMEHINA',
+        'name': 'Hello, Hologram',
+        'video': 'https://www.youtube.com/watch?v=_46UlyaJPyk',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 14,
+        'anime': 'Todoroki Hajime',
+        'name': 'Deep Dive',
+        'video': 'https://www.youtube.com/watch?v=oJj9VybmzaE',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 15,
+        'anime': 'Hoshimachi Suisei',
+        'name': 'BIBBIDIBA',
+        'video': 'https://www.youtube.com/watch?v=8ZP5eqm4JqM',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 16,
+        'anime': 'HaNaTaN',
+        'name': 'shinei',
+        'video': 'https://www.youtube.com/watch?v=Y285d_uZbXU',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 17,
+        'anime': 'yuyoyuppe feat. neko',
+        'name': 'Cut it deep',
+        'video': 'https://youtu.be/qelrzKw8ofM',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 18,
+        'anime': 'Harusaruhi',
+        'name': '(A)letheia',
+        'video': 'https://www.youtube.com/watch?v=LmgVcNNBM-4',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 19,
+        'anime': 'Ketachigai',
+        'name': 'Okiraku Luck Life',
+        'video': 'https://youtube.com/watch?v=D-ziqYnvsZQ',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 20,
+        'anime': 'Megatera Zero',
+        'name': 'Easy Song',
+        'video': 'https://youtube.com/watch?v=wFmByBkTcpQ',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 21,
+        'anime': '*Luna feat Yuaru & Nenne',
+        'name': 'Ignition',
+        'video': 'https://www.youtube.com/watch?v=6_Za11oioDY',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 22,
+        'anime': 'Nanashi Mumei',
+        'name': 'mumei',
+        'video': 'https://youtu.be/oA0CpI0vCK4?si=_Dq5JH_WhqOZk1tm',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 23,
+        'anime': 'Hoshimachi Suisei',
+        'name': 'Damn Good Day',
+        'video': 'https://youtu.be/PUJ9qXbLyfE?si=SPOMcWX5Ni1afc8s',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 24,
+        'anime': 'Alohaii feat. vally.exe',
+        'name': 'Luxury',
+        'video': 'https://youtu.be/qRu49gpb6eQ?si=gDIqhUXIsif1eHN7',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 25,
+        'anime': 'NEUN',
+        'name': 'BlueDiver',
+        'video': 'https://www.youtube.com/watch?v=djkKLb9_eF8',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 26,
+        'anime': 'rejection & KO3 (feat.yosumi)',
+        'name': 'SET IT OFF',
+        'video': 'https://www.youtube.com/watch?v=Vancxm2zO88',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 27,
+        'anime': 'yungcirnooo x remilia bandxz',
+        'name': 'like diddy',
+        'video': 'https://www.youtube.com/watch?v=m47RdwIm3c0',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 28,
+        'anime': 'CONA',
+        'name': 'Mushoku Natsu',
+        'video': 'https://www.youtube.com/watch?v=5EwYD9Q3VeY',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 29,
+        'anime': 'ha tsutsuyu',
+        'name': 'sun',
+        'video': 'https://www.youtube.com/watch?v=o2_5VQqIixU',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 30,
+        'anime': 'CULUA',
+        'name': 'Jibun Gokko',
+        'video': 'https://www.youtube.com/watch?v=pjiTdkZ9DoI',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 31,
+        'anime': 'HoneyWorks Guest Vocal',
+        'name': 'Heart Bouquet',
+        'video': 'https://www.youtube.com/watch?v=SZfI0atkrfU',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 32,
+        'anime': 'Kotoha',
+        'name': 'Catching me, Catching you',
+        'video': 'https://www.youtube.com/watch?v=fJ3ZBU03vws',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 33,
+        'anime': 'Minato Aqua',
+        'name': '#Aquairo Palette',
+        'video': 'https://www.youtube.com/watch?v=6bnaBnd4kyU',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 34,
+        'anime': 'Vestia Zeta',
+        'name': 'Break It Down',
+        'video': 'https://youtu.be/q4CY4XY-06Q?si=4_pccjLUIuAOOj8d',
+        'mp3': 'https://files.catbox.moe/1o61wl.mp3',
+        'full': null
+    },
+    {
+        'id': 35,
+        'anime': 'Tsunomaki Watame',
+        'name': 'Revival',
+        'video': 'https://youtu.be/XfiZhU2bOtg',
+        'mp3': 'https://files.catbox.moe/wk37qo.mp3',
+        'full': null
+    },
+    {
+        'id': 36,
+        'anime': 'Ookami Mio feat. Nakiri Ayame',
+        'name': 'Yoru to Ame',
+        'video': 'https://youtu.be/_sQYKoory1w?si=rnLaZ3lSoOXwm4jz',
+        'mp3': 'https://files.catbox.moe/nv0s64.mp3',
+        'full': null
+    },
+    {
+        'id': 37,
+        'anime': '*Luna feat. Nenne',
+        'name': 'Aria',
+        'video': 'https://www.youtube.com/watch?v=jKBXJXUeAeE',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 38,
+        'anime': 'Ariabl\'eyeS feat. Risa Yuzuki',
+        'name': 'Meimei Memento',
+        'video': 'https://www.youtube.com/watch?v=CLq01JgO5tA',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 39,
+        'anime': 'Imy feat. Itsuki Natsume',
+        'name': 'Uso wa Shinjitsu wo Enjiru',
+        'video': 'https://www.youtube.com/watch?v=XYxT0Np6X3k',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 40,
+        'anime': 'Setono Toto',
+        'name': 'Aotoki',
+        'video': 'https://youtu.be/Sjb3I4XD9gw?si=VqhLpbj0prHL0Lbj',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 41,
+        'anime': 'HoshiHo',
+        'name': 'Whispers of the Blue Star',
+        'video': 'https://youtu.be/oFsQeJ_C_Vw?si=kLusLm4d_u0XGPcG',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 42,
+        'anime': 'BOMBING',
+        'name': 'Nothing',
+        'video': 'https://youtu.be/NesDeg1786c?si=u6CsnenpPQohgfFx',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 43,
+        'anime': 'Hakos Baelz',
+        'name': 'EIEN',
+        'video': 'https://youtu.be/xGgKKwc-jWg?si=3Woj_UcQGFT9MJUf',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 44,
+        'anime': 'Fiona Clearesta ft. Nyxtoria',
+        'name': 'Ankoku-gai Kioku ~Fated Rendezvous~ (Midnight ver.)',
+        'video': 'https://youtu.be/fcX5KG4_C4k?si=LbMzO5b5TmqFrF7a',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 45,
+        'anime': 'tapiocaalice',
+        'name': 'Elaria et Tenebrae (Reimagined Ver)',
+        'video': 'https://youtu.be/E1iF28AQe80?si=FT4LHSQvFDDBIRrX',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 46,
+        'anime': 'ROSE CUE 9 feat. Maisaki Berry',
+        'name': 'Left Behind',
+        'video': 'https://www.youtube.com/watch?v=ypex3XueGeM',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 47,
+        'anime': 'Akesato',
+        'name': 'Tasogare ni Ou Kaze no Na wa',
+        'video': 'https://www.youtube.com/watch?v=pezr6-O3kvU',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 48,
+        'anime': 'PanaYusa',
+        'name': 'Irislight',
+        'video': 'https://www.youtube.com/watch?v=nzCYoE2nlRk',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 49,
+        'anime': 'Moona Hoshinova ft. Nerissa Ravencroft',
+        'name': '100%',
+        'video': 'https://www.youtube.com/watch?v=qm1RqoX3PzM',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 50,
+        'anime': 'ASU',
+        'name': 'Slowly',
+        'video': 'https://www.youtube.com/watch?v=enFLXbbXWdk',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 51,
+        'anime': 'HIMEHINA',
+        'name': 'Fukigen na Three Card',
+        'video': 'https://www.youtube.com/watch?v=ulH8MUUAOu4',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 52,
+        'anime': 'Harusaruhi',
+        'name': 'Prince',
+        'video': 'https://www.youtube.com/watch?v=eBdlV5qbIVc',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 53,
+        'anime': 'Mikeneko',
+        'name': 'you & me',
+        'video': 'https://www.youtube.com/watch?v=CkDKIoGGafE',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 54,
+        'anime': 'Ryuusei no Sightseeing feat. Asahina Kon',
+        'name': 'Kalmia',
+        'video': 'https://www.youtube.com/watch?v=E-j6JFikiLg',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 55,
+        'anime': 'Shishigami Leona',
+        'name': 'Mirai no Zanshi',
+        'video': 'https://www.youtube.com/watch?v=y18yplubt00',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 56,
+        'anime': 'Panno Mimimi, KOTONOHOUSE',
+        'name': 'Take You Higher',
+        'video': 'https://www.youtube.com/watch?v=yGuL5KGbG-I',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 57,
+        'anime': 'KANKAN, yowanecity',
+        'name': 'Curtain Call',
+        'video': 'https://www.youtube.com/watch?v=LqYE2xf5e2M',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 58,
+        'anime': 'Hebi',
+        'name': 'Become',
+        'video': 'https://www.youtube.com/watch?v=XPqXU3SfxdU',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 59,
+        'anime': 'KYMA',
+        'name': 'Little Me',
+        'video': 'https://www.youtube.com/watch?v=paCZO7G3YKE',
+        'mp3': null,
+        'full': null
+    },
+    {
+        'id': 60,
+        'anime': 'La prière',
+        'name': 'Aftertide',
+        'video': 'https://www.youtube.com/watch?v=xNaU0EaZrqY',
+        'mp3': null,
+        'full': null
+    }
 ] satisfies Song[];
