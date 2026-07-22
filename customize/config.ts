ae06bc225b9fd8b3adb7467145f484124ec131ab
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2026-07-22 20:18:07 +0200

    yuuko natsuyoshi

--- customize/config.ts
+++ customize/config.ts
@@ -1,11 +1,11 @@
 import type { AppConfig } from '../src/app/types';
 
 export const config = {
-    localStoragePrefix: 'test',
-    title: 'test! Sorter',
-    description: 'Party rank sorter for your custom list of songs.',
-    tags: ['test'],
-    deadline: new Date('2026-06-30T23:59:00+02:00'),
+    localStoragePrefix: 'yuuko-natsuyoshi',
+    title: 'Yuuko Natsuyoshi',
+    description: 'Party rank sorter for Yuuko Natsuyoshi.',
+    tags: ['Artist'],
+    deadline: new Date('2026-08-17T23:59:00+02:00'),
     googleSheets: {
         clientId: '575550662002-hivobiln683gua375ss3b7k58afnn36t.apps.googleusercontent.com',
         appId: '575550662002',
--- customize/songList.ts
+++ customize/songList.ts
@@ -3,34 +3,348 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {'id': 1, 'anime': 'Bang Dream', 'name': 'Asu no Yozora Shoukaihan by Afterglow', 'video': 'https://www.youtube.com/watch?v=1JgfLP2QtZw', 'mp3': null},
-    {'id': 2, 'anime': 'Bang Dream', 'name': 'Ave Mujica by Ave Mujica', 'video': 'https://youtu.be/QDsd0nyzwz0', 'mp3': null},
-    {'id': 3, 'anime': 'Bang Dream', 'name': 'Bad Kids All Bet by RAISE A SUILEN', 'video': 'https://www.youtube.com/watch?v=mKt2u5a3-H8', 'mp3': null},
-    {'id': 4, 'anime': 'Bang Dream', 'name': 'Beautiful Birthday by RAISE A SUILEN', 'video': 'https://www.youtube.com/watch?v=AxEUw6LdHR4', 'mp3': null},
-    {'id': 5, 'anime': 'Bang Dream', 'name': 'Choir \'s\' Choir by Ave Mujica', 'video': 'https://youtu.be/7llj4mh4L-8', 'mp3': null},
-    {'id': 6, 'anime': 'Bang Dream', 'name': 'Double Rainbow by Poppin\'Party', 'video': 'https://www.youtube.com/watch?v=sNaJuqFB-_g', 'mp3': null},
-    {'id': 7, 'anime': 'Bang Dream', 'name': 'Ether by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=z6k7YIIZ6Hk', 'mp3': null},
-    {'id': 8, 'anime': 'Bang Dream', 'name': 'Freedom by Poppin\'Party x Misato Fukuen', 'video': 'https://www.youtube.com/watch?v=oy6OCnr9eAg', 'mp3': null},
-    {'id': 9, 'anime': 'Bang Dream', 'name': 'Hey-day Capriccio by Afterglow', 'video': 'https://www.youtube.com/watch?v=czHlLBls4oo', 'mp3': null},
-    {'id': 10, 'anime': 'Bang Dream', 'name': 'Kamisama, Baka by Ave Mujica', 'video': 'https://youtu.be/vmheOfqsH_o', 'mp3': null},
-    {'id': 11, 'anime': 'Bang Dream', 'name': 'KiLLKiSS by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=RexyoaXaQ1o', 'mp3': null},
-    {'id': 12, 'anime': 'Bang Dream', 'name': 'Kimi ga Inakucha, Hello Happy World', 'video': 'https://naedist.animemusicquiz.com/8poyjw.webm', 'mp3': null},
-    {'id': 13, 'anime': 'Bang Dream', 'name': 'Mugen My World by Mugendai Mewtype', 'video': 'https://youtube.com/watch?v=GB2MEvY2sQk', 'mp3': null},
-    {'id': 14, 'anime': 'Bang Dream', 'name': 'Radiate Warriors by Pastel＊Palettes', 'video': 'https://youtu.be/dS-SUwftzmM', 'mp3': null},
-    {'id': 15, 'anime': 'Bang Dream', 'name': 'Returns by Poppin\'Party', 'video': 'https://www.youtube.com/watch?v=zWKV5yudE18', 'mp3': null},
-    {'id': 16, 'anime': 'Bang Dream', 'name': 'Roku-chou Nen to Ichiya Monogatari by Roselia', 'video': 'https://www.youtube.com/watch?v=xnlJkmRhi3Q', 'mp3': null},
-    {'id': 17, 'anime': 'Bang Dream', 'name': 'Sing Alive by Roselia', 'video': 'https://naedist.animemusicquiz.com/2h5qt3.webm', 'mp3': null},
-    {'id': 18, 'anime': 'Bang Dream', 'name': 'Mayoi Uta by MyGO!!!!!', 'video': 'https://naedist.animemusicquiz.com/vppf6y.webm', 'mp3': null},
-    {'id': 19, 'anime': 'Bang Dream', 'name': 'Smiling & Singing A Song by Hello, Happy World!', 'video': 'https://youtu.be/XFv3P-fKhWw', 'mp3': null},
-    {'id': 20, 'anime': 'Bang Dream', 'name': 'Song I am. by Roselia', 'video': 'https://naedist.animemusicquiz.com/njc8p5.webm', 'mp3': null},
-    {'id': 21, 'anime': 'Bang Dream', 'name': 'Sora no Música by Ave Mujica', 'video': 'https://youtu.be/2Bo-ULStqLo', 'mp3': null},
-    {'id': 22, 'anime': 'Bang Dream', 'name': 'Sprechchor by Roselia', 'video': 'https://www.youtube.com/watch?v=sdRwP4OjVn4', 'mp3': null},
-    {'id': 23, 'anime': 'Bang Dream', 'name': 'Sunlit Musical - Roselia', 'video': 'https://youtu.be/BtG1mA_1e0s', 'mp3': null},
-    {'id': 24, 'anime': 'Bang Dream', 'name': 'swim by MyGO!!!!!', 'video': 'https://www.youtube.com/watch?v=AEZ7suhPML0', 'mp3': null},
-    {'id': 25, 'anime': 'Bang Dream', 'name': 'Symbol III: ▽ by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=NB3PxWLn9v4', 'mp3': null},
-    {'id': 26, 'anime': 'Bang Dream', 'name': 'Symbol IV :Earth by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=WqQPM0dZpXc', 'mp3': null},
-    {'id': 27, 'anime': 'Bang Dream', 'name': 'Telepathy by Mugendai Mewtype', 'video': 'https://www.youtube.com/watch?v=GN_0T7cClGM', 'mp3': null},
-    {'id': 28, 'anime': 'Bang Dream', 'name': 'The Circle of Butterflies by Morfonica x fhána', 'video': 'https://www.youtube.com/watch?v=JfhgKn1hnnk', 'mp3': null},
-    {'id': 29, 'anime': 'Bang Dream', 'name': 'Utakotoba by MyGO!!!!!', 'video': 'https://youtu.be/wJ-OebTVyvk', 'mp3': null},
-    {'id': 30, 'anime': 'Bang Dream', 'name': 'Yakusoku by Roselia', 'video': 'https://naedist.animemusicquiz.com/h0rza6.webm', 'mp3': null},
+    {
+        'id': 1,
+        'anime': 'Gal to Kyouryuu',
+        'name': '"Kyouryuu Agemizawa☆" by Kyouryuu Friends',
+        'video': 'https://naedist.animemusicquiz.com/ctktf1.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/0l3qoi.mp3',
+        'full': null
+    },
+    {
+        'id': 2,
+        'anime': 'Assault Lily Bouquet',
+        'name': '"Edel Lilie" by Hitotsuyanagi-tai',
+        'video': 'https://naedist.animemusicquiz.com/sjnx78.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/4fy3lf.mp3',
+        'full': null
+    },
+    {
+        'id': 3,
+        'anime': 'Assault Lily Bouquet',
+        'name': '"Heart+Heart" by Hikaru Akao & Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/e15v2x.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/u2zifb.mp3',
+        'full': null
+    },
+    {
+        'id': 4,
+        'anime': 'Assault Lily Bouquet',
+        'name': '"GROWING*" by Hitotsuyanagi-tai',
+        'video': 'https://naedist.animemusicquiz.com/0sy34d.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/7gxbid.mp3',
+        'full': null
+    },
+    {
+        'id': 5,
+        'anime': 'Assault Lily Bouquet',
+        'name': '"Kimi no Te o Hanasanai" by Hitotsuyanagi-tai',
+        'video': 'https://naedist.animemusicquiz.com/uy1lda.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/gswr2h.mp3',
+        'full': null
+    },
+    {
+        'id': 6,
+        'anime': 'Show By Rock!! Mashumairesh!!',
+        'name': '"Hiromenes" by Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/en9up2.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/f2f3y5.mp3',
+        'full': null
+    },
+    {
+        'id': 7,
+        'anime': 'Show By Rock!! Mashumairesh!!',
+        'name': '"Kimi no Rhapsody" by Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/rz8rzi.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/y5citf.mp3',
+        'full': null
+    },
+    {
+        'id': 8,
+        'anime': 'Show By Rock!! Mashumairesh!!',
+        'name': '"Yell and Response" by Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/2b6m7p.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/xv2ke1.mp3',
+        'full': null
+    },
+    {
+        'id': 9,
+        'anime': 'Show By Rock!! Mashumairesh!!',
+        'name': '"Platform" by Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/4l92tq.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/s6fhrn.mp3',
+        'full': null
+    },
+    {
+        'id': 10,
+        'anime': 'Show By Rock!! Stars!!',
+        'name': '"Do Re Mi Fa STARS!!" by Plasmagica & Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/mdox9z.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/8fy3ti.mp3',
+        'full': null
+    },
+    {
+        'id': 11,
+        'anime': 'Show By Rock!! Stars!!',
+        'name': '"Hoshizora Light Story" by Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/gwvynp.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/5u4w9l.mp3',
+        'full': null
+    },
+    {
+        'id': 12,
+        'anime': 'Show By Rock!! Stars!!',
+        'name': '"Speed Up" by Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/xneo4h.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/yggyle.mp3',
+        'full': null
+    },
+    {
+        'id': 13,
+        'anime': 'Show By Rock!! Stars!!',
+        'name': '"Runner\'s High!!" by Plasmagica & Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/ccj9bw.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/48r1xo.mp3',
+        'full': null
+    },
+    {
+        'id': 14,
+        'anime': 'Show By Rock!! Stars!!',
+        'name': '"Anokanatarium" by Mashumairesh!!',
+        'video': 'https://naedist.animemusicquiz.com/szzegb.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/y7euh8.mp3',
+        'full': null
+    },
+    {
+        'id': 15,
+        'anime': 'Sakura Kakumei: Hana Saku Otome-tachi',
+        'name': '"SAKURA HIKARU Revolution" by Teikoku Kageki-dan',
+        'video': 'https://naedist.animemusicquiz.com/gxca1j.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/nsxxfy.mp3',
+        'full': null
+    },
+    {
+        'id': 16,
+        'anime': 'Assault Lily Fruits',
+        'name': '"Can\'t Stop Fruity" by Hikaru Akao & Yuuko Natsuyoshi & Ayaka Fujii & Kaori Maeda',
+        'video': 'https://naedist.animemusicquiz.com/8hg6e0.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/tecxwe.mp3',
+        'full': null
+    },
+    {
+        'id': 17,
+        'anime': 'Build-Divide: #000000 (Code Black)',
+        'name': '"Poison☆SUMMER!!!" by Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/ufakbr.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/x9tnba.mp3',
+        'full': null
+    },
+    {
+        'id': 18,
+        'anime': 'Shine Post',
+        'name': '"Wonder Starter" by TINGS',
+        'video': 'https://naedist.animemusicquiz.com/ke8wiz.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/19bz2e.mp3',
+        'full': null
+    },
+    {
+        'id': 19,
+        'anime': 'Shine Post',
+        'name': '"Palette Girls" by TINGS',
+        'video': 'https://naedist.animemusicquiz.com/lmhvsh.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/29y925.mp3',
+        'full': null
+    },
+    {
+        'id': 20,
+        'anime': 'Shine Post',
+        'name': '"Ippo Mae no Sekai" by TiNgS',
+        'video': 'https://naedist.animemusicquiz.com/89b08d.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/a4o2pm.mp3',
+        'full': null
+    },
+    {
+        'id': 21,
+        'anime': 'Shine Post',
+        'name': '"Yellow Rose" by TiNgS',
+        'video': 'https://naedist.animemusicquiz.com/g5b0e2.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/792pez.mp3',
+        'full': null
+    },
+    {
+        'id': 22,
+        'anime': 'Shine Post',
+        'name': '"Be Your Light!!" by TINGS',
+        'video': 'https://naedist.animemusicquiz.com/y5patt.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/2zp993.mp3',
+        'full': null
+    },
+    {
+        'id': 23,
+        'anime': 'Shine Post',
+        'name': '"TOKYO WATASHI COLLECTION" by TiNgS',
+        'video': 'https://naedist.animemusicquiz.com/4qcpls.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/stusgn.mp3',
+        'full': null
+    },
+    {
+        'id': 24,
+        'anime': 'Shine Post',
+        'name': '"On Your Mark!!" by TINGS',
+        'video': 'https://naedist.animemusicquiz.com/qmhq11.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/zgcsah.mp3',
+        'full': null
+    },
+    {
+        'id': 25,
+        'anime': 'Kunoichi Tsubaki no Mune no Uchi',
+        'name': '"Akane-gumi Katsudou Nisshi ~Inu-han~" by Yuuko Natsuyoshi・Miyari Nemoto・Sayumi Suzushiro',
+        'video': 'https://naedist.animemusicquiz.com/puu6ca.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/uuct3v.mp3',
+        'full': null
+    },
+    {
+        'id': 26,
+        'anime': 'Kunoichi Tsubaki no Mune no Uchi',
+        'name': '"Akane-gumi Katsudou Nisshi ~Neushitorautatsumiumahitsujisarutoriinui Daishuugou!~" by Neushitorautatsumiumahitsujisarutoriinui',
+        'video': 'https://naedist.animemusicquiz.com/ydzpb9.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/0yv9w6.mp3',
+        'full': null
+    },
+    {
+        'id': 27,
+        'anime': 'Uma Musume: Pretty Derby Season 3',
+        'name': '"Soshite Minna no" by Hinaki Yano, Hina Tachibana, Sayumi Suzushiro, Yuuko Natsuyoshi, MAKIKO, Akina',
+        'video': 'https://naedist.animemusicquiz.com/eur9tk.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/idn1wq.mp3',
+        'full': null
+    },
+    {
+        'id': 28,
+        'anime': 'Uma Musume: Pretty Derby Season 3',
+        'name': '"Uma Pyoi Densetsu" by Uma Musume 3',
+        'video': 'https://naedist.animemusicquiz.com/fpxo6v.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/ntjorf.mp3',
+        'full': null
+    },
+    {
+        'id': 29,
+        'anime': 'Uma Musume: Pretty Derby Season 3',
+        'name': '"Hikari no Ushirosugata" by Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/zardy9.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/ybj2qw.mp3',
+        'full': null
+    },
+    {
+        'id': 30,
+        'anime': 'Uma Musume: Pretty Derby Season 3',
+        'name': '"Soshite Minna no" by Hinaki Yano & Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/bmvf0r.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/i5ta2f.mp3',
+        'full': null
+    },
+    {
+        'id': 31,
+        'anime': 'Unnamed Memory',
+        'name': '"blan_" by Arika',
+        'video': 'https://naedist.animemusicquiz.com/3wu8xf.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/vuod9w.mp3',
+        'full': null
+    },
+    {
+        'id': 32,
+        'anime': 'Utagoe wa Mille-Feuille',
+        'name': '"Omoidebanashi" by Temarizawa Koukou Acapella-bu',
+        'video': 'https://naedist.animemusicquiz.com/922s39lbxc66qvfp.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/k8bz3v.mp3',
+        'full': null
+    },
+    {
+        'id': 33,
+        'anime': 'Utagoe wa Mille-Feuille',
+        'name': '"Garnet -Acapella Arrange ver.-" by Temarizawa Koukou Acapella-bu',
+        'video': 'https://naedist.animemusicquiz.com/830bbx4bin98yt4c.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/xo7iyz.mp3',
+        'full': null
+    },
+    {
+        'id': 34,
+        'anime': 'Utagoe wa Mille-Feuille',
+        'name': '"Yumemiru Fifteen -Acapella Arrange ver.-" by Temarizawa Koukou Acapella-bu',
+        'video': 'https://naedist.animemusicquiz.com/ymd8zjgihla8pe4r.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/ryda8d.mp3',
+        'full': null
+    },
+    {
+        'id': 35,
+        'anime': 'Unnamed Memory',
+        'name': '"inclusion" by Arika',
+        'video': 'https://naedist.animemusicquiz.com/giukuw.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/yyfhay.mp3',
+        'full': null
+    },
+    {
+        'id': 36,
+        'anime': 'Chou Kaguya-hime!',
+        'name': '"ray Chou Kaguya-hime! Version" by Yuuko Natsuyoshi & Saori Hayami',
+        'video': 'https://naedist.animemusicquiz.com/ke7zw59edfzlvyar.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/a4gmac.mp3',
+        'full': null
+    },
+    {
+        'id': 37,
+        'anime': 'Chou Kaguya-hime!',
+        'name': '"Melt CPK! Remix" by ryo (supercell) feat. Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/9a49e4fwczs86qjz.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/biv6t2.mp3',
+        'full': null
+    },
+    {
+        'id': 38,
+        'anime': 'Chou Kaguya-hime!',
+        'name': '"Watashi wa, Watashi no Koto ga Suki." by Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/ke7xno59vzgffo97.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/21x5nj.mp3',
+        'full': null
+    },
+    {
+        'id': 39,
+        'anime': 'Chou Kaguya-hime!',
+        'name': '"Happy Synthesizer (COVER)" by Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/ijsnd9gwpu54mcqt.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/13d5en.mp3',
+        'full': null
+    },
+    {
+        'id': 40,
+        'anime': 'Chou Kaguya-hime!',
+        'name': '"World is Mine CPK! Remix" by ryo (supercell) feat. Yuuko Natsuyoshi & Saori Hayami',
+        'video': 'https://naedist.animemusicquiz.com/bk6so464rykpyl8k.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/w5kjwg.mp3',
+        'full': null
+    },
+    {
+        'id': 41,
+        'anime': 'Chou Kaguya-hime!',
+        'name': '"Ex-Otogibanashi" by Yuuko Natsuyoshi & Saori Hayami',
+        'video': 'https://naedist.animemusicquiz.com/hg5z70jybj0o0vxf.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/tekb50.mp3',
+        'full': null
+    },
+    {
+        'id': 42,
+        'anime': 'Chou Kaguya-hime!',
+        'name': '"Shunkan, Symphony." by Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/tk0be8wif7c1nhcy.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/yr0sio.mp3',
+        'full': null
+    },
+    {
+        'id': 43,
+        'anime': 'Chou Kaguya-hime!',
+        'name': '"Reply" by Yuuko Natsuyoshi',
+        'video': 'https://naedist.animemusicquiz.com/34uqf6cuc4v16a20.webm',
+        'mp3': 'https://naedist.animemusicquiz.com/1ll2qx.mp3',
+        'full': null
+    }
 ] satisfies Song[];
