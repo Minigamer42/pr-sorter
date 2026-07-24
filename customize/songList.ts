f3f612b455bccd42c8e49b6479b380de65b2ede6
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2026-07-24 19:06:39 +0200

    idol pr 14

--- customize/songList.ts
+++ customize/songList.ts
@@ -3,34 +3,252 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {'id': 1, 'anime': 'Bang Dream', 'name': 'Asu no Yozora Shoukaihan by Afterglow', 'video': 'https://www.youtube.com/watch?v=1JgfLP2QtZw', 'mp3': null},
-    {'id': 2, 'anime': 'Bang Dream', 'name': 'Ave Mujica by Ave Mujica', 'video': 'https://youtu.be/QDsd0nyzwz0', 'mp3': null},
-    {'id': 3, 'anime': 'Bang Dream', 'name': 'Bad Kids All Bet by RAISE A SUILEN', 'video': 'https://www.youtube.com/watch?v=mKt2u5a3-H8', 'mp3': null},
-    {'id': 4, 'anime': 'Bang Dream', 'name': 'Beautiful Birthday by RAISE A SUILEN', 'video': 'https://www.youtube.com/watch?v=AxEUw6LdHR4', 'mp3': null},
-    {'id': 5, 'anime': 'Bang Dream', 'name': 'Choir \'s\' Choir by Ave Mujica', 'video': 'https://youtu.be/7llj4mh4L-8', 'mp3': null},
-    {'id': 6, 'anime': 'Bang Dream', 'name': 'Double Rainbow by Poppin\'Party', 'video': 'https://www.youtube.com/watch?v=sNaJuqFB-_g', 'mp3': null},
-    {'id': 7, 'anime': 'Bang Dream', 'name': 'Ether by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=z6k7YIIZ6Hk', 'mp3': null},
-    {'id': 8, 'anime': 'Bang Dream', 'name': 'Freedom by Poppin\'Party x Misato Fukuen', 'video': 'https://www.youtube.com/watch?v=oy6OCnr9eAg', 'mp3': null},
-    {'id': 9, 'anime': 'Bang Dream', 'name': 'Hey-day Capriccio by Afterglow', 'video': 'https://www.youtube.com/watch?v=czHlLBls4oo', 'mp3': null},
-    {'id': 10, 'anime': 'Bang Dream', 'name': 'Kamisama, Baka by Ave Mujica', 'video': 'https://youtu.be/vmheOfqsH_o', 'mp3': null},
-    {'id': 11, 'anime': 'Bang Dream', 'name': 'KiLLKiSS by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=RexyoaXaQ1o', 'mp3': null},
-    {'id': 12, 'anime': 'Bang Dream', 'name': 'Kimi ga Inakucha, Hello Happy World', 'video': 'https://naedist.animemusicquiz.com/8poyjw.webm', 'mp3': null},
-    {'id': 13, 'anime': 'Bang Dream', 'name': 'Mugen My World by Mugendai Mewtype', 'video': 'https://youtube.com/watch?v=GB2MEvY2sQk', 'mp3': null},
-    {'id': 14, 'anime': 'Bang Dream', 'name': 'Radiate Warriors by Pastel＊Palettes', 'video': 'https://youtu.be/dS-SUwftzmM', 'mp3': null},
-    {'id': 15, 'anime': 'Bang Dream', 'name': 'Returns by Poppin\'Party', 'video': 'https://www.youtube.com/watch?v=zWKV5yudE18', 'mp3': null},
-    {'id': 16, 'anime': 'Bang Dream', 'name': 'Roku-chou Nen to Ichiya Monogatari by Roselia', 'video': 'https://www.youtube.com/watch?v=xnlJkmRhi3Q', 'mp3': null},
-    {'id': 17, 'anime': 'Bang Dream', 'name': 'Sing Alive by Roselia', 'video': 'https://naedist.animemusicquiz.com/2h5qt3.webm', 'mp3': null},
-    {'id': 18, 'anime': 'Bang Dream', 'name': 'Mayoi Uta by MyGO!!!!!', 'video': 'https://naedist.animemusicquiz.com/vppf6y.webm', 'mp3': null},
-    {'id': 19, 'anime': 'Bang Dream', 'name': 'Smiling & Singing A Song by Hello, Happy World!', 'video': 'https://youtu.be/XFv3P-fKhWw', 'mp3': null},
-    {'id': 20, 'anime': 'Bang Dream', 'name': 'Song I am. by Roselia', 'video': 'https://naedist.animemusicquiz.com/njc8p5.webm', 'mp3': null},
-    {'id': 21, 'anime': 'Bang Dream', 'name': 'Sora no Música by Ave Mujica', 'video': 'https://youtu.be/2Bo-ULStqLo', 'mp3': null},
-    {'id': 22, 'anime': 'Bang Dream', 'name': 'Sprechchor by Roselia', 'video': 'https://www.youtube.com/watch?v=sdRwP4OjVn4', 'mp3': null},
-    {'id': 23, 'anime': 'Bang Dream', 'name': 'Sunlit Musical - Roselia', 'video': 'https://youtu.be/BtG1mA_1e0s', 'mp3': null},
-    {'id': 24, 'anime': 'Bang Dream', 'name': 'swim by MyGO!!!!!', 'video': 'https://www.youtube.com/watch?v=AEZ7suhPML0', 'mp3': null},
-    {'id': 25, 'anime': 'Bang Dream', 'name': 'Symbol III: ▽ by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=NB3PxWLn9v4', 'mp3': null},
-    {'id': 26, 'anime': 'Bang Dream', 'name': 'Symbol IV :Earth by Ave Mujica', 'video': 'https://www.youtube.com/watch?v=WqQPM0dZpXc', 'mp3': null},
-    {'id': 27, 'anime': 'Bang Dream', 'name': 'Telepathy by Mugendai Mewtype', 'video': 'https://www.youtube.com/watch?v=GN_0T7cClGM', 'mp3': null},
-    {'id': 28, 'anime': 'Bang Dream', 'name': 'The Circle of Butterflies by Morfonica x fhána', 'video': 'https://www.youtube.com/watch?v=JfhgKn1hnnk', 'mp3': null},
-    {'id': 29, 'anime': 'Bang Dream', 'name': 'Utakotoba by MyGO!!!!!', 'video': 'https://youtu.be/wJ-OebTVyvk', 'mp3': null},
-    {'id': 30, 'anime': 'Bang Dream', 'name': 'Yakusoku by Roselia', 'video': 'https://naedist.animemusicquiz.com/h0rza6.webm', 'mp3': null},
+    {
+        'id': 1,
+        'anime': '=LOVE',
+        'name': '"Madamada Minor? My Number Card" by =LOVE',
+        'video': 'https://www.youtube.com/watch?v=93iIH0_e1QM',
+        'mp3': 'https://www.youtube.com/watch?v=93iIH0_e1QM',
+        'full': null
+    },
+    {
+        'id': 2,
+        'anime': 'Assault Lily',
+        'name': '"Chou yo Hana yo ja Irarenai" by Odaiba Girls\' High School',
+        'video': 'https://www.youtube.com/watch?v=175dyLChxIA',
+        'mp3': 'https://www.youtube.com/watch?v=175dyLChxIA',
+        'full': null
+    },
+    {
+        'id': 3,
+        'anime': 'Assault Lily',
+        'name': '"Multicolored Flowers" by Gran Eple',
+        'video': 'https://www.youtube.com/watch?v=FsNz4n4JR9o',
+        'mp3': 'https://www.youtube.com/watch?v=FsNz4n4JR9o',
+        'full': null
+    },
+    {
+        'id': 4,
+        'anime': 'Assault Lily',
+        'name': '"OVERFLOW" by Hitotsuyanagi-tai',
+        'video': 'https://www.youtube.com/watch?v=yCKbkeXzVao',
+        'mp3': 'https://www.youtube.com/watch?v=yCKbkeXzVao',
+        'full': null
+    },
+    {
+        'id': 5,
+        'anime': 'CHARMS!!',
+        'name': '"DECIDE" by Memento',
+        'video': 'https://www.youtube.com/watch?v=ZWFF8RNSagc',
+        'mp3': 'https://www.youtube.com/watch?v=ZWFF8RNSagc',
+        'full': null
+    },
+    {
+        'id': 6,
+        'anime': 'Cho Tokimeki Sendenbu',
+        'name': '"Acchi Muite Kyun" by Cho Tokimeki Sendenbu',
+        'video': 'https://www.youtube.com/watch?v=kXQZdCssAV0',
+        'mp3': 'https://files.catbox.moe/c2wmbd.flac',
+        'full': null
+    },
+    {
+        'id': 7,
+        'anime': 'Cho Tokimeki Sendenbu',
+        'name': '"JIRI JIRI" by Cho Tokimeki Sendenbu',
+        'video': 'https://www.youtube.com/watch?v=MqrN2JsLF4I',
+        'mp3': 'https://files.catbox.moe/4mvq5x.flac',
+        'full': null
+    },
+    {
+        'id': 8,
+        'anime': 'Dear❤Vocalist',
+        'name': '"2 of a Kind" by Souma Saitou',
+        'video': 'https://www.youtube.com/watch?v=CMIv8kqn7GU',
+        'mp3': 'https://www.youtube.com/watch?v=CMIv8kqn7GU',
+        'full': null
+    },
+    {
+        'id': 9,
+        'anime': 'Gakuen iDOLM@STER',
+        'name': '"Kachidoki" by Hikaru Iida',
+        'video': 'https://www.youtube.com/watch?v=T6X2MREymGM',
+        'mp3': 'https://www.youtube.com/watch?v=T6X2MREymGM',
+        'full': null
+    },
+    {
+        'id': 10,
+        'anime': 'Gakuen iDOLM@STER',
+        'name': '"Rironbusou Shite" by Yukari Amane',
+        'video': 'https://www.youtube.com/watch?v=0ZWUdwVQOJA',
+        'mp3': 'https://www.youtube.com/watch?v=lBa3J9E-3_s',
+        'full': null
+    },
+    {
+        'id': 11,
+        'anime': 'Girls²',
+        'name': '"The Finest" by Girls²×iScream',
+        'video': 'https://www.youtube.com/watch?v=JU_KToTjP0o',
+        'mp3': 'https://www.youtube.com/watch?v=YNXYcnbcl0E',
+        'full': null
+    },
+    {
+        'id': 12,
+        'anime': 'Harusaruhi',
+        'name': '"Echo" by Harusaruhi',
+        'video': 'https://www.youtube.com/watch?v=QxmUPvXtofE',
+        'mp3': 'https://www.youtube.com/watch?v=QxmUPvXtofE',
+        'full': null
+    },
+    {
+        'id': 13,
+        'anime': 'Ibuki',
+        'name': '"No title" by Ibuki',
+        'video': 'https://www.youtube.com/watch?v=9tPL1YKyAik',
+        'mp3': 'https://www.youtube.com/watch?v=9tPL1YKyAik',
+        'full': null
+    },
+    {
+        'id': 14,
+        'anime': 'iDOL Street',
+        'name': '"Stand Alone・Complex" by Wa-suta',
+        'video': 'https://www.youtube.com/watch?v=sr3yX1SdKIg',
+        'mp3': 'https://www.youtube.com/watch?v=gUH1vN2CRtI',
+        'full': null
+    },
+    {
+        'id': 15,
+        'anime': 'iLiFE!',
+        'name': '"Kyawa Poppin’ Doo" by iLiFE!',
+        'video': 'https://www.youtube.com/watch?v=4FOSHKro21k',
+        'mp3': 'https://www.youtube.com/watch?v=4FOSHKro21k',
+        'full': null
+    },
+    {
+        'id': 16,
+        'anime': 'Love Live! Nijigasaki Gakuen School Idol Doukoukai',
+        'name': '"Silent Blaze" by Akari Kitou',
+        'video': 'https://www.youtube.com/watch?v=RdwPV7Mv-4w',
+        'mp3': 'https://www.youtube.com/watch?v=RdwPV7Mv-4w',
+        'full': null
+    },
+    {
+        'id': 17,
+        'anime': 'Love Live! Sunshine!!',
+        'name': '"Deep Blue" by Aqours',
+        'video': 'https://www.youtube.com/watch?v=093lrnBMM_Q',
+        'mp3': 'https://www.youtube.com/watch?v=093lrnBMM_Q',
+        'full': null
+    },
+    {
+        'id': 18,
+        'anime': 'Love Live! Superstar!!',
+        'name': '"Day1" by Liella!',
+        'video': 'https://www.youtube.com/watch?v=pgYY9LkoJv8',
+        'mp3': 'https://www.youtube.com/watch?v=pgYY9LkoJv8',
+        'full': null
+    },
+    {
+        'id': 19,
+        'anime': 'Mahoutsukai to Kuroneko no Wiz',
+        'name': '"Relation (Ballad ver.)" by Ruriko Noguchi',
+        'video': 'https://file.garden/alwN97Y5rARmvcVO/relation.mp4',
+        'mp3': 'https://file.garden/alwN97Y5rARmvcVO/relation.mp3',
+        'full': null
+    },
+    {
+        'id': 20,
+        'anime': 'may in film',
+        'name': '"Starlight" by may in film',
+        'video': 'https://www.youtube.com/watch?v=EtUoCl8UnTY',
+        'mp3': 'https://www.youtube.com/watch?v=GIhVWdBXjLg',
+        'full': null
+    },
+    {
+        'id': 21,
+        'anime': 'O.N.G.E.K.I.',
+        'name': '"Event Horizon" by Anzu Haruno',
+        'video': 'https://www.youtube.com/watch?v=W-10yAf_1uo',
+        'mp3': 'https://www.youtube.com/watch?v=W-10yAf_1uo',
+        'full': null
+    },
+    {
+        'id': 22,
+        'anime': 'Ongaku Shoujo',
+        'name': '"Kaze ni Naru" by Lynn',
+        'video': 'https://www.youtube.com/watch?v=yVaJZds_mdE',
+        'mp3': 'https://www.youtube.com/watch?v=yVaJZds_mdE',
+        'full': null
+    },
+    {
+        'id': 23,
+        'anime': 'Pimm\'s',
+        'name': '"BEST WISHES" by Pimm\'s',
+        'video': 'https://www.youtube.com/watch?v=VO0r61dcRHY',
+        'mp3': 'https://www.youtube.com/watch?v=eNrm1mLlbFw',
+        'full': null
+    },
+    {
+        'id': 24,
+        'anime': 'Roppongi Sadistic Night',
+        'name': '"Orange no Sora to Kizuna" by Aya Yamane & Emi Hirayama',
+        'video': 'https://www.youtube.com/watch?v=oiPgtsAPbmE',
+        'mp3': 'https://www.youtube.com/watch?v=oiPgtsAPbmE',
+        'full': null
+    },
+    {
+        'id': 25,
+        'anime': 'SHOW BY ROCK!!',
+        'name': '"Cinematic Mémoire" by Aya Yamane',
+        'video': 'https://www.youtube.com/watch?v=1_a0XJE6Lc4',
+        'mp3': 'https://www.youtube.com/watch?v=1_a0XJE6Lc4',
+        'full': null
+    },
+    {
+        'id': 26,
+        'anime': 'THE iDOLM@STER Cinderella Girls',
+        'name': '"O-Ku-Ri-Mo-No Sunday!" by Rika Nagae & Hina Tachibana',
+        'video': 'https://www.youtube.com/watch?v=ykt7nHHyNqg',
+        'mp3': 'https://www.youtube.com/watch?v=ykt7nHHyNqg',
+        'full': null
+    },
+    {
+        'id': 27,
+        'anime': 'THE iDOLM@STER Shiny Colors',
+        'name': '"Sleepless Nights" by ALSTROEMERIA',
+        'video': 'https://www.youtube.com/watch?v=2Dmjx6f47oU',
+        'mp3': 'https://www.youtube.com/watch?v=2Dmjx6f47oU',
+        'full': null
+    },
+    {
+        'id': 28,
+        'anime': 'THE iDOLM@STER Shiny Colors',
+        'name': '"Sparkle" by illumination STARS×noctchill',
+        'video': 'https://www.youtube.com/watch?v=xw1Pvd5bsjU',
+        'mp3': 'https://www.youtube.com/watch?v=Fl3WSCUOJtY',
+        'full': null
+    },
+    {
+        'id': 29,
+        'anime': 'Tokyo 7th Sisters',
+        'name': '"NATSUKAGE" by 777☆SISTERS',
+        'video': 'https://www.youtube.com/watch?v=OVJ4asJeRoQ',
+        'mp3': 'https://www.youtube.com/watch?v=OVJ4asJeRoQ',
+        'full': null
+    },
+    {
+        'id': 30,
+        'anime': 'Tsukipro',
+        'name': '"No winter lasts forever." by pioniX',
+        'video': 'https://www.youtube.com/watch?v=llRyVEdOZ7Y',
+        'mp3': 'https://www.youtube.com/watch?v=llRyVEdOZ7Y',
+        'full': null
+    },
+    {
+        'id': 31,
+        'anime': 'World Dai Star',
+        'name': '"Zettai! Niedlich Sengen♡" by Sally Amaki & Maria Naganawa',
+        'video': 'https://www.youtube.com/watch?v=0GI00nflN44',
+        'mp3': 'https://www.youtube.com/watch?v=9t7r9_rSuwk',
+        'full': null
+    }
 ] satisfies Song[];
